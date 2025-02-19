cece3524102919c21f9c14490455ffa462ecd4d3
paccvvet <120043593+paccvvet@users.noreply.github.com>
2025-02-19 16:55:05 +0300

    feat: api request

--- tik-talk/src/app/app.component.ts
+++ tik-talk/src/app/app.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import { ProfileService } from './data/services/profile.service';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ProfileCardComponent } from './common-ui/profile-card/profile-card.component';
+import { Profile } from './data/interfaces/profile.interface';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProfileCardComponent],
+  imports: [RouterOutlet, ProfileCardComponent, CommonModule],
 
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'tik-talk';
+  ProfileService = inject(ProfileService);
+  profiles: Profile[] = [];
+  constructor() {
+    this.ProfileService.getTestAccounts().subscribe((val) => {
+      this.profiles = val;
+    });
+  }
 }
