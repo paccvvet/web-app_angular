a65122ed4a5aef48162b71d53825a292f4ef1c3f
paccvvet <120043593+paccvvet@users.noreply.github.com>
2025-02-19 17:55:22 +0300

    fix: style profile-card

--- tik-talk/src/app/app.component.ts
+++ tik-talk/src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { ProfileService } from './data/services/profile.service';
 import { Component, inject } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ProfileCardComponent } from './common-ui/profile-card/profile-card.component';
 import { Profile } from './data/interfaces/profile.interface';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProfileCardComponent, CommonModule],
+  imports: [RouterOutlet, ProfileCardComponent],
 
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
--- tik-talk/src/app/common-ui/profile-card/profile-card.component.ts
+++ tik-talk/src/app/common-ui/profile-card/profile-card.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Profile } from './../../data/interfaces/profile.interface';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-profile-card',
   imports: [],
   templateUrl: './profile-card.component.html',
-  styleUrl: './profile-card.component.scss'
+  styleUrl: './profile-card.component.scss',
 })
 export class ProfileCardComponent {
-
+  @Input() profile!: Profile;
 }
