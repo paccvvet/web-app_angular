d6d1fa5d2585aa2189f8a6e11ab8d5949d703ae9
paccvvet <120043593+paccvvet@users.noreply.github.com>
2025-02-20 14:45:20 +0300

    feat: single page application

--- tik-talk/src/app/app.component.ts
+++ tik-talk/src/app/app.component.ts
@@ -1,23 +1,11 @@
-import { ProfileService } from './data/services/profile.service';
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ProfileCardComponent } from './common-ui/profile-card/profile-card.component';
-import { Profile } from './data/interfaces/profile.interface';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProfileCardComponent],
+  imports: [RouterOutlet],
 
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'tik-talk';
-  ProfileService = inject(ProfileService);
-  profiles: Profile[] = [];
-  constructor() {
-    this.ProfileService.getTestAccounts().subscribe((val) => {
-      this.profiles = val;
-    });
-  }
-}
+export class AppComponent {}
