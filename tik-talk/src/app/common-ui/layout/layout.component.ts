8597ae9f585cd05f5592548e9a63b490c74f4aa7
paccvvet <120043593+paccvvet@users.noreply.github.com>
2025-02-21 20:50:52 +0300

    feat: login-page

--- tik-talk/src/app/common-ui/layout/layout.component.ts
+++ tik-talk/src/app/common-ui/layout/layout.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SideBarComponent } from '../side-bar/side-bar.component';
 
 @Component({
   selector: 'app-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SideBarComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
 })
