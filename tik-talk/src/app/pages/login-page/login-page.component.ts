9cb9ee9d590baa562d02721cf63867cdd2016f99
paccvvet <120043593+paccvvet@users.noreply.github.com>
2025-02-23 15:36:10 +0300

    feat: authServise, fix: loginPage form

--- tik-talk/src/app/pages/login-page/login-page.component.ts
+++ tik-talk/src/app/pages/login-page/login-page.component.ts
@@ -1,5 +1,11 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from './../../auth/auth.service';
+import { Component, inject } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
@@ -8,12 +14,19 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
   styleUrl: './login-page.component.scss',
 })
 export class LoginPageComponent {
+  AuthService = inject(AuthService);
   form = new FormGroup({
-    username: new FormControl(null),
-    password: new FormControl(null),
+    username: new FormControl(null, Validators.required),
+    password: new FormControl(null, Validators.required),
   });
 
   onSubmit(event: Event) {
     console.log(this.form.value);
+    if (this.form.valid) {
+      //@ts-ignore
+      this.AuthService.login(this.form.value).subscribe((res) => {
+        console.log(res);
+      });
+    }
   }
 }
